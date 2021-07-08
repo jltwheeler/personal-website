8f8c7f14693b6c93ad11f8d5b8c92589fae92ec4
Josh Wheeler <jltwheeler@gmail.com>
2021-07-08 22:42:42 +0100

    fixes for static asset pathing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { prefix } from '../config';
 
 export default function Home() {
   return (
@@ -59,7 +60,12 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <img
+              src={`${prefix}/vercel.svg`}
+              alt="Vercel Logo"
+              width={72}
+              height={16}
+            />
           </span>
         </a>
       </footer>
