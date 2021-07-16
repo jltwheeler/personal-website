14334caccf5ac4573967d7c4c35ae1c1fbccd4f6
Josh Wheeler <jltwheeler@gmail.com>
2021-07-16 15:08:52 +0100

    initial blog page

---
+++ pages/blog/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import styled from 'styled-components';
+
+import { Footer, Hero, Navbar } from '../../components';
+import { ThemeProps } from '../../config';
+
+const MainStyle = styled.main`
+  min-height: 100vh;
+  overflow: hidden;
+  display: block;
+  position: relative;
+  padding-bottom: 5rem;
+
+  @media (max-width: ${(props: ThemeProps) => props.theme.sizes['s']}px) {
+    padding-bottom: 10rem;
+  }
+`;
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Josh Wheeler | Blog</title>
+        <meta name="description" content="Josh Wheeler's personal tech blog." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <MainStyle>
+        <Navbar />
+        <Hero />
+        <Footer />
+      </MainStyle>
+    </>
+  );
+}
