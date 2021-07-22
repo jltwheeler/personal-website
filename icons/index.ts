0d7115506e580515690c530765661c0e86730f90
Josh Wheeler <jltwheeler@gmail.com>
2021-07-22 23:37:36 +0100

    add important svg icon

--- icons/index.ts
+++ icons/index.ts
@@ -2,4 +2,5 @@ export { Cross } from './cross';
 export { GitHub } from './github';
 export { Gmail } from './gmail';
 export { Hamburger } from './hamburger';
+export { Important } from './important';
 export { LinkedIn } from './linkedin';
